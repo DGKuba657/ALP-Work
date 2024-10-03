197e35b1a2c1f591b5de2be224ca371ad6f500be
DG_Kuba <jakubfunk90@gmail.com>
2024-10-03 10:59:42 +0200

    OOP
    Remake into OOP

--- Spoj textu/Program.cs
+++ Spoj textu/Program.cs
@@ -1,52 +1,26 @@
-﻿
-while (true)
-{
-    Console.WriteLine("Choose what you wanna use 1 = spojovac textu or 2 = absolutni hodnota 3 = soucet des cisel 4 = delka stringu");
-    string userInput = Console.ReadLine();
-
-    switch (userInput)
-    {
-        case"1":
-            Console.WriteLine("Napis svoji prvni cast textu");
-            string string1 = Console.ReadLine();
-
-            Console.WriteLine("Napis svoji druhou cast textu");
-            string string2 = Console.ReadLine();
-
-
-            string completion = string1 + string2;
-            Console.WriteLine($"Tvuj text jest {completion}");
-            break;
-        case "2":
-            Console.WriteLine("Napis cislo");
-            float number1 = float.Parse(Console.ReadLine());
-            float absolutni_hodnota = Math.Abs(number1);
-            Console.WriteLine(absolutni_hodnota);
-            break;
-        case "3":
-            Console.WriteLine("Napis cislo");
-            float floatNumber = float.Parse(Console.ReadLine());
-            Console.WriteLine("Napis druhe cislo");
-            float floatNumber2 = float.Parse(Console.ReadLine());
-            float result = floatNumber + floatNumber2;
-            Console.WriteLine($"Tvuj vysledek je {result}");
-            break;
-        
-        case "4":
-        Console.WriteLine("Napis text");
-            string text1 = Console.ReadLine();
-            Console.WriteLine(text1.Length);      
-            break;
-        
-        default: Console.WriteLine("Wrong input"); break;
+﻿namespace Spoj_Textu {
+    public static class Program {
+        public static void Main() {
+            Console.WriteLine("What do you want to run? 1 = Spojovac Textu  2 = absolutni hodnota 3 = scitani 4 = Delka stringu");
+            string userinput = Console.ReadLine();
+            switch (userinput)
+            {
+                case "1":
+                    Spoj.SpojovacTextu();
+                    break;
+                case "2":
+                    Spoj.Absolute();
+                    break;
+                case "3":
+                    Spoj.Scitac();
+                    break;
+                case "4":
+                    Spoj.TextLength();
+                    break;
+            }
+        }
     }
-    Thread.Sleep(3000);
-    Console.Clear();
-    
 }
-
-
-
 
 
 
---
+++ Spoj textu/Spoj.cs
@@ -0,0 +1,42 @@
+﻿namespace SpojovacTextu;
+
+public class Spoj
+{
+    public static void SpojovacTextu()
+    {
+        Console.WriteLine("Napis svoji prvni cast textu");
+        string string1 = Console.ReadLine();
+
+        Console.WriteLine("Napis svoji druhou cast textu");
+        string string2 = Console.ReadLine();
+
+
+        string completion = string1 + string2;
+        Console.WriteLine($"Tvuj text jest {completion}");
+    }
+
+    public static void Absolute()
+    {
+        Console.WriteLine("Napis cislo");
+        float number1 = float.Parse(Console.ReadLine());
+        float absolutni_hodnota = Math.Abs(number1);
+        Console.WriteLine(absolutni_hodnota);
+    }
+
+    public static void Scitac()
+    {
+        Console.WriteLine("Napis cislo");
+        float floatNumber = float.Parse(Console.ReadLine());
+        Console.WriteLine("Napis druhe cislo");
+        float floatNumber2 = float.Parse(Console.ReadLine());
+        float result = floatNumber + floatNumber2;
+        Console.WriteLine($"Tvuj vysledek je {result}");
+    }
+
+    public static void TextLength()
+    {
+        Console.WriteLine("Napis text");
+        string text1 = Console.ReadLine();
+        Console.WriteLine(text1.Length);
+    }
+}
