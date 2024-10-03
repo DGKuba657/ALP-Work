def606f95142fd59a27e167d407faa1940d483b4
DG_Kuba <93142095+DGKuba657@users.noreply.github.com>
2024-10-03 11:42:17 +0200

    Program.cs

--- Spoj textu/Program.cs
+++ Spoj textu/Program.cs
@@ -1,23 +1,38 @@
-﻿namespace Spoj_Textu {
+namespace Spoj_Textu {
     public static class Program {
         public static void Main() {
-            Console.WriteLine("What do you want to run? 1 = Spojovac Textu  2 = absolutni hodnota 3 = scitani 4 = Delka stringu");
-            string userinput = Console.ReadLine();
-            switch (userinput)
+            bool running = true;
+            while (running)
             {
-                case "1":
-                    Spoj.SpojovacTextu();
-                    break;
-                case "2":
-                    Spoj.Absolute();
-                    break;
-                case "3":
-                    Spoj.Scitac();
-                    break;
-                case "4":
-                    Spoj.TextLength();
-                    break;
+                Console.WriteLine("What do you want to run? 1 = Spojovac Textu  2 = absolutni hodnota 3 = scitani 4 = Delka stringu");
+                string userinput = Console.ReadLine();
+                switch (userinput)
+                {
+                    case "1":
+                        Spoj.SpojovacTextu();
+                        break;
+                    case "2":
+                        Spoj.Absolute();
+                        break;
+                    case "3":
+                        Spoj.Scitac();
+                        break;
+                    case "4":
+                        Spoj.TextLength();
+                        break;
+                }
+                Thread.Sleep(1500);
+                Console.WriteLine("Do you want to continue? (y/n)");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "no" || input.ToLower() == "n")
+                {
+                    running = false;
+                }
+                Console.Clear();
             }
+            
+            
         }
     }
 }
